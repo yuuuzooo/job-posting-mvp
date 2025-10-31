05ce9d89d8ed5758c6e5791ad0feb3719a4ac4e0
yuuuzooo <free.yuya.m@gmail.com>
2025-11-01 01:01:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 
